c784a31a2c52f79d0cf5ee1aac210ffc0fe78eb5
s18620 <61684587+s18620@users.noreply.github.com>
2020-03-09 15:26:09 +0100

    v0.11
    Transform Bug

--- Potato_Unity/Assets/FollowPlayer.cs
+++ Potato_Unity/Assets/FollowPlayer.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     public GameObject Player;
-     
+    public Vector3 offset = new Vector3(0, 0,-10);
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +15,6 @@ public class FollowPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        rb = Player.transform.position;
+        transform.position = new Vector3(Player.transform.position.x + offset.x, Player.transform.position.y + offset.y, Player.transform.position.z + offset.z);
     }
 }
